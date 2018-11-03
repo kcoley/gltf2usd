baa8784fdbcf7625a308f75bc5798ef77438f568
Kacey Coley <kcoley@users.noreply.github.com>
2018-11-02 23:37:16 -0700

    added alpha mode (#93)

--- Source/_gltf2usd/version.py
+++ Source/_gltf2usd/version.py
@@ -3,7 +3,7 @@ class Version(object):
     """
     _major = 0
     _minor = 1
-    _patch = 4
+    _patch = 5
     @staticmethod
     def get_major_version_number():
         """Returns the major version
