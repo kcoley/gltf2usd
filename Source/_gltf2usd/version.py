027b5ccec44fc1b0ce30d17127087b84b16d7a5b
Kacey Coley <kcoley@users.noreply.github.com>
2018-12-04 23:57:08 -0800

    handle vector4 vertex color (#114)
    * updating info on vertex color
    
    * updated changelog

--- Source/_gltf2usd/version.py
+++ Source/_gltf2usd/version.py
@@ -3,7 +3,7 @@ class Version(object):
     """
     _major = 0
     _minor = 1
-    _patch = 10
+    _patch = 11
     @staticmethod
     def get_major_version_number():
         """Returns the major version
