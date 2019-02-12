bd287a97a20bfb407c44e2c6ace6d96fd62b37c9
Kacey Coley <kcoley@g.clemson.edu>
2019-02-11 21:52:05 -0800

    update regex to check for square brackets

--- Source/_gltf2usd/version.py
+++ Source/_gltf2usd/version.py
@@ -3,7 +3,7 @@ class Version(object):
     """
     _major = 0
     _minor = 1
-    _patch = 20
+    _patch = 21
     @staticmethod
     def get_major_version_number():
         """Returns the major version
